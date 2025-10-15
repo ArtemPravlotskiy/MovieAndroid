82d6affda14e7a6206ba01da1c5c41e664f48df1
trend <pravlotskiy.artem24@gmail.com>
2025-10-15 22:44:31 +0300

    Make start screen (not ready)

--- app/src/main/java/com/example/movies/ui/StartScreen.kt
+++ app/src/main/java/com/example/movies/ui/StartScreen.kt
@@ -1,2 +1,78 @@
 package com.example.movies.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.ContentType
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.movies.ui.theme.MoviesTheme
+import com.example.movies.R
+
+@Composable
+fun StartScreen (
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(R.drawable.main),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = modifier.fillMaxSize()
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom,
+            modifier = modifier.fillMaxSize()
+            ) {
+            Button(
+                onClick = {}, // TODO click activity
+                shape = RoundedCornerShape(16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Black,
+                    contentColor = Color.White
+                ),
+                modifier = Modifier.height(130.dp).width(270.dp).padding(10.dp)
+            ) {
+                Text(text = "Start") // TODO stringres
+            }
+            Button(
+                onClick = {}, // TODO click activity
+                shape = RoundedCornerShape(16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Black,
+                    contentColor = Color.White
+                ),
+                modifier = Modifier.height(130.dp).width(270.dp).padding(10.dp)
+            ) {
+                Text(text = "Settings") // TODO stringres
+            }
+            Spacer(modifier = Modifier.height(69.dp))
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun StartScreenPreview() {
+    MoviesTheme {
+        StartScreen()
+    }
+}
