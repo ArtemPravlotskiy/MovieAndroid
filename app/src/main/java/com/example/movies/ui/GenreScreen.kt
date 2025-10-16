d94f6f2117c6db5c2b812afe9eded648a7622de5
trend <pravlotskiy.artem24@gmail.com>
2025-10-17 00:27:25 +0300

    Start genre screen

--- app/src/main/java/com/example/movies/ui/GenreScreen.kt
+++ app/src/main/java/com/example/movies/ui/GenreScreen.kt
@@ -1,2 +1,31 @@
 package com.example.movies.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.movies.R
+import com.example.movies.ui.theme.MoviesTheme
+
+@Composable
+fun GenreScreen (
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(R.drawable.genres),
+            contentDescription = null
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun GenreScreenPreview() {
+    MoviesTheme {
+        GenreScreen()
+    }
+}
