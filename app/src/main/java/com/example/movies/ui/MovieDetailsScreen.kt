bf2875fb28370941f159a2bbd095bc41c3e71bc9
trend <pravlotskiy.artem24@gmail.com>
2025-10-23 17:06:23 +0300

    Rename file

--- app/src/main/java/com/example/movies/ui/MovieDetailsScreen.kt
+++ app/src/main/java/com/example/movies/ui/MovieDetailsScreen.kt
@@ -1,14 +1,11 @@
 package com.example.movies.ui
 
-import android.view.RoundedCorner
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -23,20 +20,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import com.example.movies.data.mockMoviesResponse
-import com.example.movies.model.Movie
 import com.example.movies.R
 import com.example.movies.model.MovieDetails
 
