92cba5528db9aba1266eeda2a080f1f9e5dda5f2
trend <pravlotskiy.artem24@gmail.com>
2025-10-23 16:13:57 +0300

    Create movie details view model

---
+++ app/src/main/java/com/example/movies/viewModel/MovieDetailsViewModel.kt
@@ -0,0 +1,60 @@
+package com.example.movies.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.movies.MoviesApplication
+import com.example.movies.data.MovieDetailsRepository
+import com.example.movies.model.MovieDetails
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
+
+sealed interface MovieDetailsUiState {
+    data class Success(val movieDetails: MovieDetails) : MovieDetailsUiState
+    object Error : MovieDetailsUiState
+    object Loading : MovieDetailsUiState
+}
+
+class MovieDetailsViewModel (
+    private val movieDetailsRepository: MovieDetailsRepository
+) : ViewModel() {
+    private val _movieDetailsUiState = MutableStateFlow<MovieDetailsUiState>(MovieDetailsUiState.Loading)
+    val movieDetailsUiState: StateFlow<MovieDetailsUiState> = _movieDetailsUiState
+
+    private val movieId: String = ""
+
+    init {
+        getMovieDetails()
+    }
+
+    fun getMovieDetails() {
+        viewModelScope.launch {
+            _movieDetailsUiState.value = MovieDetailsUiState.Loading
+            _movieDetailsUiState.value = try {
+                MovieDetailsUiState.Success(movieDetailsRepository
+                    .getMovieDetails(movieId = movieId)
+                )
+            } catch (e: IOException) {
+                MovieDetailsUiState.Error
+            } catch (e: HttpException) {
+                MovieDetailsUiState.Error
+            }
+        }
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as MoviesApplication)
+                val movieDetailsRepository = application.container.movieDetailsRepository
+                MovieDetailsViewModel(movieDetailsRepository = movieDetailsRepository)
+            }
+        }
+    }
+}
