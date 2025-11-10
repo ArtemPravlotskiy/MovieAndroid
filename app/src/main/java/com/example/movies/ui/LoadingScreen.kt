ebea709898548afe840d51d4397cc28aef577c42
trend <pravlotskiy.artem24@gmail.com>
2025-11-11 01:08:15 +0300

    Centrize loading screen

--- app/src/main/java/com/example/movies/ui/LoadingScreen.kt
+++ app/src/main/java/com/example/movies/ui/LoadingScreen.kt
@@ -1,8 +1,11 @@
 package com.example.movies.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -13,9 +16,15 @@ import com.example.movies.R
 fun LoadingScreen(
     modifier: Modifier = Modifier
 ) {
-    Image(
-        painter = painterResource(R.drawable.loading_img),
-        contentDescription = stringResource(R.string.loading),
-        modifier = modifier.size(200.dp)
-    )
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+        )
+    {
+        Image(
+            painter = painterResource(R.drawable.loading_img),
+            contentDescription = stringResource(R.string.loading),
+            modifier = modifier.size(200.dp)
+        )
+    }
 }
